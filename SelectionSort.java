9e28d1bc3f8693e95f7968650e29d7cbc6cda5dc
meraihan <sayedmahmudraihan@gmail.com>
2019-12-28 20:42:22 +0600

    selection sort complexity adds and details explanation

--- SelectionSort.java
+++ SelectionSort.java
@@ -32,3 +32,25 @@ public class SelectionSort {
       st.printArray(arr);
     }
 }
+
+// Time Complexity O(n2) as there are two nested loops.
+// https://www.geeksforgeeks.org/wp-content/uploads/Selection-sort-flowchart.jpg
+
+
+// arr[] = 64 25 12 22 11
+
+// Find the minimum element in arr[0...4]
+// and place it at beginning
+// 11 25 12 22 64
+
+// Find the minimum element in arr[1...4]
+// and place it at beginning of arr[1...4]
+// 11 12 25 22 64
+
+// Find the minimum element in arr[2...4]
+// and place it at beginning of arr[2...4]
+// 11 12 22 25 64
+
+// Find the minimum element in arr[3...4]
+// and place it at beginning of arr[3...4]
+// 11 12 22 25 64 
