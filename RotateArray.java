057ea11a268a06be4b34a935d5945c0a2790b241
meraihan <sayedmahmudraihan@gmail.com>
2020-01-01 02:03:11 -0700

    temporary variable changed to rotateOnebyOne Method

--- RotateArray.java
+++ RotateArray.java
@@ -9,11 +9,11 @@ public class RotateArray {
     void leftRotatebyOne(int arr[], int n) 
     { 
         int i, temp; 
-        temp = arr[0]; 
-        for (i = 0; i < n - 1; i++) {
-            arr[i] = arr[i + 1]; 
-        }
-        arr[i] = temp; 
+         for (i = 0; i < n - 1; i++){
+           temp = arr[i];
+           arr[i] = arr[i + 1]; 
+           arr[i+1] = temp;   
+        } 
     } 
   
     /* utility function to print an array */
